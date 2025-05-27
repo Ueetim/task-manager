cd458d7c7ab134f3a8e75627ad9fa7a298fe5c4a
Uduak Etim <lorduduak@gmail.com>
2025-05-27 20:35:11 +0100

    chore(frontend): initialize angular app

---
+++ task-manager-client/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+
+@Component({
+  selector: 'app-root',
+  imports: [
+    ButtonModule
+  ],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'task-manager-client';
+}
